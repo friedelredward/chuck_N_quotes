5777ca8411a9f2f8153197ae0ee50466308e3ace
R Edward Friedel <redwardweb@gmail.com>
2023-08-08 00:10:07 +0200

    initial structure

---
+++ src/main/java/com/example/chuck_n_quotes/ChuckNQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.chuck_n_quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chuck_n_quotes/ChuckNQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chuck_n_quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
